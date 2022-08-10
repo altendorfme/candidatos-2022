3aa3cc8d532f0599edf36f395a57dfd6b138d304
altendorfme <altendorfme@gmail.com>
2022-08-10 20:13:08 -0300

    remove last update, this is wrong value on tse page

--- json.php
+++ json.php
@@ -35,70 +35,54 @@ if ($dados_abertos === FALSE) {
 	$dom->loadHTML($dados_abertos);
 	libxml_clear_errors();
 	$xpath = new DOMXPath($dom);
-	$verificar_atualizacao = $xpath->query('//*[@id="content"]//span[@class="automatic-local-datetime"]/@data-datetime');
 
-	if (file_exists('tse/' . ANO . '/ultima_atualizacao')) {
-		$ultima_atualizacao = file_get_contents(ANO . '/ultima_atualizacao');
-	} else {
-		$ultima_atualizacao = null;
-	}
+	$file = 'https://cdn.tse.jus.br/estatistica/sead/odsele/consulta_cand/consulta_cand_' . ANO . '.zip';
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $file);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36');
+	$consulta_candidato = curl_exec($ch);
+	curl_close($ch);
 
-	if ($ultima_atualizacao === $verificar_atualizacao[0]->nodeValue) {
-		echo 'Arquivos do TSE sem atualização' . PHP_EOL;
+	if ($consulta_candidato === FALSE) {
+		echo 'Erro ao baixar o ZIP' . PHP_EOL;
 		die();
 	} else {
-		$ultima_atualizacao = strftime('%d de %B de %Y às %H:%M:%S', strtotime($verificar_atualizacao[0]->nodeValue));
-		echo 'Arquivos do TSE atualizados: ' . $ultima_atualizacao . PHP_EOL;
+		file_put_contents(ANO . '/tse.zip', $consulta_candidato);
 
-		$file = 'https://cdn.tse.jus.br/estatistica/sead/odsele/consulta_cand/consulta_cand_' . ANO . '.zip';
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $file);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36');
-		$consulta_candidato = curl_exec($ch);
-		curl_close($ch);
-
-		if ($consulta_candidato === FALSE) {
-			echo 'Erro ao baixar o ZIP' . PHP_EOL;
-			die();
+		$zip = new ZipArchive;
+		if ($zip->open(ANO . '/tse.zip') === TRUE) {
+			$zip->extractTo(ANO . '/csv/');
+			$zip->close();
+			echo 'Arquivos extraidos' . PHP_EOL;
+			unlink(ANO . '/tse.zip');
+			unlink(ANO . '/csv/leiame.pdf');
+			unlink(ANO . '/csv/consulta_cand_' . ANO . '_BRASIL.csv');
 		} else {
-			file_put_contents(ANO . '/tse.zip', $consulta_candidato);
+			echo 'Erro ao extrair' . PHP_EOL;
+			die();
+		}
 
-			$zip = new ZipArchive;
-			if ($zip->open(ANO . '/tse.zip') === TRUE) {
-				$zip->extractTo(ANO . '/csv/');
-				$zip->close();
-				echo 'Arquivos extraidos' . PHP_EOL;
-				unlink(ANO . '/tse.zip');
-				unlink(ANO . '/csv/leiame.pdf');
-				unlink(ANO . '/csv/consulta_cand_' . ANO . '_BRASIL.csv');
+		foreach (ESTADOS as $uf => $state) {
+			echo 'UF: ' . strtoupper($uf) . PHP_EOL;
+			unlink(ANO . '/csv/'. $uf . '.csv');
+
+			$candidatos = ANO . '/csv/consulta_cand_' . ANO . '_' . strtoupper($uf) . '.csv';
+			rename($candidatos, ANO . '/csv/'. $uf . '.csv');
+			$candidatos = ANO . '/csv/'. $uf . '.csv';
+			if (file_exists($candidatos)) {
+				$candidatos = mb_convert_encoding(file_get_contents($candidatos), 'UTF-8', 'US-ASCII');
+				$csv = new \ParseCsv\Csv();
+				$csv->auto($candidatos);
+
+				$json = json_encode($csv->data, JSON_UNESCAPED_UNICODE);
+				file_put_contents(ANO . '/json/' . $uf . '.json', $json);
 			} else {
-				echo 'Erro ao extrair' . PHP_EOL;
+				echo 'Erro ao processar CSV de ' . strtoupper($uf) . PHP_EOL;
 				die();
 			}
-
-			foreach (ESTADOS as $uf => $state) {
-				echo 'UF: ' . strtoupper($uf) . PHP_EOL;
-				unlink(ANO . '/csv/'. $uf . '.csv');
-
-				$candidatos = ANO . '/csv/consulta_cand_' . ANO . '_' . strtoupper($uf) . '.csv';
-				rename($candidatos, ANO . '/csv/'. $uf . '.csv');
-				$candidatos = ANO . '/csv/'. $uf . '.csv';
-				if (file_exists($candidatos)) {
-					$candidatos = mb_convert_encoding(file_get_contents($candidatos), 'UTF-8', 'US-ASCII');
-					$csv = new \ParseCsv\Csv();
-					$csv->auto($candidatos);
-
-					$json = json_encode($csv->data, JSON_UNESCAPED_UNICODE);
-					file_put_contents(ANO . '/json/' . $uf . '.json', $json);
-				} else {
-					echo 'Erro ao processar CSV de ' . strtoupper($uf) . PHP_EOL;
-					die();
-				}
-			}
-			file_put_contents(ANO . '/ultima_atualizacao', $verificar_atualizacao[0]->nodeValue);
 		}
 	}
 }
